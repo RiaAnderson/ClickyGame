2810bc579badde2a43822fd45e3ff6fb0f7aadbd
Ria <astoriaa92@gmail.com>
2020-01-14 23:35:43 -0800

    added image size css

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,10 +1,56 @@
 import React, {Component} from 'react';
+import './styles.css';
 
 class App extends Component {
   state={
     actors:[
       {
-        images: require ("./images/Alexis.jpg")
+        images: require ("./images/Alexis.jpg"),
+        id: 1
+      },
+      {
+        images: require ("./images/David.jpg"),
+        id: 2
+      },
+      {
+        images: require ("./images/Jocelyn.jpg"),
+        id: 3
+      },
+      {
+        images: require ("./images/Johnny.jpg"),
+        id: 4
+      },
+      {
+        images: require ("./images/Moira.jpg"),
+        id: 5
+      },
+      {
+        images: require ("./images/Mutt.jpg"),
+        id: 6
+      },
+      {
+        images: require ("./images/Patrick.jpg"),
+        id: 7
+      },
+      {
+        images: require ("./images/Roland.jpg"),
+        id: 8
+      },
+      {
+        images: require ("./images/Ronnie.jpg"),
+        id: 9
+      },
+      {
+        images: require ("./images/Stevie.jpg"),
+        id: 10
+      },
+      {
+        images: require ("./images/Ted.jpg"),
+        id: 11
+      },
+      {
+        images: require ("./images/Twyla.jpeg"),
+        id: 12
       }
     ]
   }
@@ -15,7 +61,10 @@ class App extends Component {
     <div className="App">
       <h1>Schitt's Creek Memory Game</h1>
       <p>Click all the cast members to score points, but don't click on a cast member more than once.</p>
-      {this.state.actors.map(actor => <img src={actor.images} alt="placeholder" />)}
+      <div className="cast">
+      {this.state.actors.map(actor => <img key={actor.id} src={actor.images} alt="cast member" />)}
+      </div>
+      
       
     </div>
   );
