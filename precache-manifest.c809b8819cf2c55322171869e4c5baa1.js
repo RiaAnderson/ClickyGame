9329ff7bb140cf58cc670d25f185c9cd038286f5
Ria <astoriaa92@gmail.com>
2020-01-18 11:04:56 -0800

    Updates

---
+++ precache-manifest.c809b8819cf2c55322171869e4c5baa1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c149ed8e0461ac9cbd7a3dc3ca18f4b0",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "c1caecc6d5b354c06536",
+    "url": "/ClickyGame/static/css/main.573b6e43.chunk.css"
+  },
+  {
+    "revision": "24e2b775cdc01fdf92ca",
+    "url": "/ClickyGame/static/js/2.38b959f5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ClickyGame/static/js/2.38b959f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1caecc6d5b354c06536",
+    "url": "/ClickyGame/static/js/main.ca19cfdc.chunk.js"
+  },
+  {
+    "revision": "88489d381a86dae2d822",
+    "url": "/ClickyGame/static/js/runtime-main.1386749d.js"
+  },
+  {
+    "revision": "f7b18f0f0eb84575e8b6a3108e3f5eb2",
+    "url": "/ClickyGame/static/media/Alexis.f7b18f0f.jpg"
+  },
+  {
+    "revision": "323033c2268f5b318fd697229bf4e8d6",
+    "url": "/ClickyGame/static/media/David.323033c2.jpg"
+  },
+  {
+    "revision": "84d30c14c6e57a67edb52f306983e1ca",
+    "url": "/ClickyGame/static/media/Jocelyn.84d30c14.jpg"
+  },
+  {
+    "revision": "ebe20b22a8cb86d69a80ecc24c509c9b",
+    "url": "/ClickyGame/static/media/Johnny.ebe20b22.jpg"
+  },
+  {
+    "revision": "9db3af57b6562f1e8431a383a0ccc451",
+    "url": "/ClickyGame/static/media/Moira.9db3af57.jpg"
+  },
+  {
+    "revision": "62de513e3a9bf88d3ccfa752c5e7bfb4",
+    "url": "/ClickyGame/static/media/Mutt.62de513e.jpg"
+  },
+  {
+    "revision": "af5c0d476e0b861b8c8e335a73b31426",
+    "url": "/ClickyGame/static/media/Patrick.af5c0d47.jpg"
+  },
+  {
+    "revision": "8484eb054a50d283304c0059c84fe471",
+    "url": "/ClickyGame/static/media/Roland.8484eb05.jpg"
+  },
+  {
+    "revision": "07ef93317fc42aa3a6e8f3e716a02cdc",
+    "url": "/ClickyGame/static/media/Ronnie.07ef9331.jpg"
+  },
+  {
+    "revision": "be8f223e663f3edc5d2cb73647b3821b",
+    "url": "/ClickyGame/static/media/Stevie.be8f223e.jpg"
+  },
+  {
+    "revision": "8e421aca81b34e33b2ee2b1094e4c9ae",
+    "url": "/ClickyGame/static/media/Ted.8e421aca.jpg"
+  },
+  {
+    "revision": "1231b6e7da86f31fd46e0f8bb8d2731a",
+    "url": "/ClickyGame/static/media/Twyla.1231b6e7.jpeg"
+  }
+]);
